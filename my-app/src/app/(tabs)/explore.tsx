73d14090890633a2245734f49df8f116bafe0596
AhWeiHENGuEi <aa9440307@gmail.com>
2024-12-16 13:41:00 +0800

    chore: refactor explore screen and update layout; add chats index component

--- my-app/src/app/(tabs)/explore.tsx
+++ my-app/src/app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { Image, Platform, StyleSheet } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -18,7 +18,8 @@ export default function TabTwoScreen() {
           name="chevron.left.forwardslash.chevron.right"
           style={styles.headerImage}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
@@ -97,8 +98,8 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
     bottom: -90,
+    color: '#808080',
     left: -35,
     position: 'absolute',
   },
